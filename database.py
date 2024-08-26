d87049b24fc06dc20fac0804eb149c4d812f237e
Timoteo <randomthecoder1557@gmail.com>
2024-08-26 12:01:57 -0300

    feat: create function to add clients to the database
    Also adds typing to the database documents.

--- database.py
+++ database.py
@@ -1,6 +1,24 @@
 from pymongo import MongoClient
+from pymongo.collection import Collection
 from bson.objectid import ObjectId
 import bson.binary
+from typing import TypedDict
+import datetime
+
+
+class Item(TypedDict):
+    name: str
+    quantity: int
+    image: bson.binary.Binary
+
+
+class Client(TypedDict):
+    name: str
+    debt: float
+    total_installments: int
+    individual_installment_value: float
+    next_installment_payment_date: float
+    installments_left_to_pay: int
 
 
 class Database:
@@ -11,17 +29,42 @@ class Database:
 
         self.database = self.client.rita_database
 
-        self.database_items = self.database.items
+        self.database_items: Collection[Item] = self.database.items
+
+        self.clients_database: Collection[Client] = self.database.clients
+
+    def create_client(
+        self,
+        name: str,
+        debt: float,
+        total_installments: int,
+        individual_installment_value: float,
+        next_installment_payment_date: datetime.date,
+    ):
+
+        self.clients_database.insert_one(
+            {
+                "name": name,
+                "debt": debt,
+                "total_installments": total_installments,
+                "individual_installment_value": individual_installment_value,
+                "next_installment_payment_date": next_installment_payment_date,
+                "paid_installments": 0,
+                "installments_left_to_pay": total_installments,
+            }
+        )
 
     def create_item(self, name: str, quantity: int, image_path: str):
         with open(image_path, "rb") as item_image_file:
             encoded_item_image = bson.binary.Binary(item_image_file.read())
 
-        self.database_items.insert_one({
-            "name": name,
-            "quantity": quantity,
-            "image": encoded_item_image,
-        })
+        self.database_items.insert_one(
+            {
+                "name": name,
+                "quantity": quantity,
+                "image": encoded_item_image,
+            }
+        )
 
     def get_items(self):
         return self.database_items.find()
