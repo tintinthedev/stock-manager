d0c55473fafb2bb2c7b482d987da6c7641a4f422
Timoteo <randomthecoder1557@gmail.com>
2024-08-28 10:59:36 -0300

    feat: implement client data edition feature
    The data edition window opens when you click on a specific row of the clients spreadsheet (table).
    
    This also adds 2 more columns to the spreadsheet.

--- clients_spreadsheet_window.py
+++ clients_spreadsheet_window.py
@@ -1,13 +1,16 @@
 import customtkinter as ctk
-from tkinter import ttk
 from add_client_to_spreadsheet_window import AddClientToSpreadsheetWindow
 from database import Database
 from CTkTable import *
+from edit_client_data_window import EditClientDataWindow
 
 
 class ClientsSpreadsheetWindow(ctk.CTkToplevel):
     def __init__(self):
         super().__init__()
+
+        self.geometry("900x500")
+        self.attributes("-topmost", True)
 
         self.title("Planilha de clientes")
 
@@ -44,10 +47,12 @@ class ClientsSpreadsheetWindow(ctk.CTkToplevel):
             [
                 "NOME",
                 "DÍVIDA",
+                "VALOR DE CADA PARCELA",
                 "PARCELAS TOTAIS",
                 "PARCELAS EFETIVADAS",
                 "PARCELAS EM ABERTO",
                 "PRÓXIMA PARCELA",
+                "ID",
             ],
         ]
 
@@ -56,12 +61,14 @@ class ClientsSpreadsheetWindow(ctk.CTkToplevel):
         for client in clients:
             client_name = client["name"]
             client_debt = client["debt"]
+            individual_installment_value = client["individual_installment_value"]
             total_client_installments = client["total_installments"]
             paid_client_installments = client["paid_installments_date"]
             client_installments_left_to_pay = client["installments_left_to_pay_dates"]
             next_client_installment_payment_date = client[
                 "next_installment_payment_date"
             ]
+            client_id = client["_id"]
 
             paid_client_installments = "\n".join(paid_client_installments)
             client_installments_left_to_pay = "\n".join(client_installments_left_to_pay)
@@ -69,10 +76,12 @@ class ClientsSpreadsheetWindow(ctk.CTkToplevel):
             client_table_row = [
                 client_name,
                 client_debt,
+                individual_installment_value,
                 total_client_installments,
                 paid_client_installments,
                 client_installments_left_to_pay,
                 next_client_installment_payment_date,
+                client_id,
             ]
 
             table_values.append(client_table_row)
@@ -82,7 +91,11 @@ class ClientsSpreadsheetWindow(ctk.CTkToplevel):
             widget.destroy()
 
         self.table = CTkTable(
-            self.table_frame, column=6, row=table_rows, values=table_values
+            self.table_frame,
+            column=len(table_values[0]),
+            row=table_rows,
+            values=table_values,
+            command=lambda table_click_data: self.edit_client(table_click_data),
         )
 
         self.table.pack(expand=True, fill="both")
@@ -91,4 +104,16 @@ class ClientsSpreadsheetWindow(ctk.CTkToplevel):
         client_creation_window = AddClientToSpreadsheetWindow()
         self.wait_window(client_creation_window)  # waiting for the window to close
 
+        self.create_table()
+
+    def edit_client(self, table_click_data):
+        clicked_row = table_click_data["row"]
+
+        clicked_row_data = self.table.get_row(clicked_row)
+
+        edit_client_data_window = EditClientDataWindow(client_data=clicked_row_data)
+
+        self.wait_window(edit_client_data_window)
+
+        # re-creating the table to show updated data
         self.create_table()
